d906e50f65cd73a41127623594b292776e580b4b
I Gede Hari Kresna Wiyasa <harikresnagede@gmail.com>
2025-01-06 14:51:38 +0800

    feat: multi webhook send

--- src/index.ts
+++ src/index.ts
@@ -12,24 +12,34 @@ type Embed = {
   color: number;
 };
 
+type Env = Record<string, string>;
+
 export default {
   async fetch(request, env): Promise<Response> {
-    const baseWebhookMessage: WebhookMessage = {
-      username: env.DISCORD_WEBHOOK_DISPLAY_NAME ?? "Ko-fi Bot",
-      avatar_url: env.DISCORD_WEBHOOK_AVATAR_URL ?? "https://storage.ko-fi.com/cdn/useruploads/26713482-7aa8-48f3-9269-f50e74292477_d33a0b94-982a-4a4e-ac2e-dd6f28ae47ce.png",
-      attachments: [],
-      content: "",
-      embeds: [],
-    };
-
-    const baseEmbed: Embed = {
-      title: "",
-      description: "",
-      color: 0x72a5f2,
-    };
-
     switch (request.method) {
       case "POST": {
+        const webhookUrlKeys = Object.keys(env).filter((value) => value.startsWith("DISCORD_WEBHOOK_URL_"));
+        if (webhookUrlKeys.length == 0) {
+          throw new Error("no webhook urls");
+        }
+        const webhookUrls = webhookUrlKeys.map((value) => env[value]).filter((value) => value);
+
+        const webhookMessage: WebhookMessage = {
+          username: env.DISCORD_WEBHOOK_DISPLAY_NAME ?? "Ko-fi Bot",
+          avatar_url:
+            env.DISCORD_WEBHOOK_AVATAR_URL ??
+            "https://storage.ko-fi.com/cdn/useruploads/26713482-7aa8-48f3-9269-f50e74292477_d33a0b94-982a-4a4e-ac2e-dd6f28ae47ce.png",
+          attachments: [],
+          content: "",
+          embeds: [],
+        };
+
+        const baseEmbed: Embed = {
+          title: "",
+          description: "",
+          color: 0x72a5f2,
+        };
+
         // This is ridiculous... Ko-fi sends out the JSON payload with a form encoded payload.
         // The actual JSON is inside the data paramater. Why???
         const formBody = await request.formData();
@@ -37,14 +47,11 @@ export default {
         const body = JSON.parse(bodyValue);
 
         if (body["verification_token"] !== env.KOFI_VERIFICATION_TOKEN) {
-          throw new Error("Wrong verification token");
+          throw new Error("verification_token invalid");
         }
 
         const kofiEmbed = { ...baseEmbed };
-        const webhookMessage: WebhookMessage = {
-          ...baseWebhookMessage,
-          embeds: [kofiEmbed],
-        };
+        webhookMessage.embeds = [kofiEmbed];
 
         if (body["is_subscription_payment"] === true) {
           const baseTitle = body["is_first_subscription_payment"] === true ? "New first subscription payment" : "New subscription payment";
@@ -55,17 +62,18 @@ export default {
 
         kofiEmbed.description = `Donated **${body["amount"]} ${body["currency"]}**\n${body["message"]}`;
 
-        const webhookResponse = await fetch(env.DISCORD_WEBHOOK_URL, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(webhookMessage),
+        const fetches = webhookUrls.map((url) => {
+          return fetch(url, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(webhookMessage),
+          });
         });
 
-        console.log(await webhookResponse.text());
-
-        return new Response(JSON.stringify(body));
+        const results = await Promise.allSettled(fetches);
+        return new Response(JSON.stringify(results));
       }
       default:
         break;
